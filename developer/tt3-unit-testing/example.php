a4bd2bd4ea0c6de9e765e34faa011f37c1df90a8
tnsoftbear <avi-team@inbox.lv>
2022-02-17 21:25:10 +0200

    Adjust TT3

--- developer/tt3-unit-testing/example.php
+++ developer/tt3-unit-testing/example.php
@@ -4,43 +4,33 @@ include_once "./vendor/autoload.php";
 
 use Bidpath\Tt3UnitTesting\Calculator;
 
-echo "Success calculation by user amount\n";
 $result = (new Calculator())->calculate(3);
-print_r($result);
+echo $result->message() . PHP_EOL;
 
-echo "Success calculation by user and auction amount\n";
 $result = (new Calculator())->calculate(3, 2);
-print_r($result);
+echo $result->message() . PHP_EOL;
 
-echo " Success calculation by date amount\n";
 $result = (new Calculator(45))->calculate(null, null, new DateTime("2022-01-01"));
-print_r($result);
+echo $result->message() . PHP_EOL;
 
-echo "Success calculation by file\n";
 $result = (new Calculator())->calculate(null, null, null, __DIR__ . "/var/file_amount.txt");
-print_r($result);
+echo $result->message() . PHP_EOL;
 
-echo "Success calculation by random rate\n";
 $calculator = new Calculator(45, 10);
 $result = $calculator->calculate();
-print_r($result);
+echo $result->message() . PHP_EOL;
 
-echo "Fail with \"user not found\" error\n";
 $result = (new Calculator())->calculate(1);
-print_r($result);
+echo $result->message() . PHP_EOL;
 
-echo "Fail calculation, because file not found\n";
 $result = (new Calculator())->calculate(null, null, null, __DIR__ . "/var/file_absent.txt");
-print_r($result);
+echo $result->message() . PHP_EOL;
 
-echo "Fail calculation, because amount from file is invalid \n";
 $result = (new Calculator())->calculate(null, null, null, __DIR__ . "/var/file_invalid_amount.txt");
-print_r($result);
+echo $result->message() . PHP_EOL;
 
-echo "Fail calculation, because amount from file is negative \n";
 $result = (new Calculator())->calculate(null, null, null, __DIR__ . "/var/file_negative_amount.txt");
-print_r($result);
+echo $result->message() . PHP_EOL;
 
-echo "Fail, cannot found amount\n";
 $result = (new Calculator())->calculate();
-print_r($result);
+echo $result->message() . PHP_EOL;
--- developer/tt3-unit-testing/src/Result.php
+++ developer/tt3-unit-testing/src/Result.php
@@ -8,12 +8,24 @@ class Result
     public const OK_BY_DATE = 3;
     public const OK_BY_FILE = 4;
     public const OK_BY_RAND = 5;
-
     public const ERR_USER_ABSENT = 11;
     public const ERR_FILE_ABSENT = 12;
     public const ERR_FILE_AMOUNT_INVALID = 13;
     public const ERR_FILE_AMOUNT_NEGATIVE = 14;
     public const ERR_AMOUNT_NOT_FOUND = 15;
+
+    private const MESSAGES = [
+        self::OK_BY_USER => 'By user amount',
+        self::OK_BY_AUCTION_USER => 'By auction and user amount',
+        self::OK_BY_DATE => 'By date amount',
+        self::OK_BY_FILE => 'By file amount',
+        self::OK_BY_RAND => 'By random rate',
+        self::ERR_USER_ABSENT => 'User not found',
+        self::ERR_FILE_ABSENT => 'File absent',
+        self::ERR_FILE_AMOUNT_INVALID => 'File amount invalid',
+        self::ERR_FILE_AMOUNT_NEGATIVE => 'File amount negative',
+        self::ERR_AMOUNT_NOT_FOUND => 'Amount cannot be calculated',
+    ];
 
     public ?float $amount = null;
     public ?int $status = null;
@@ -27,5 +39,13 @@ class Result
     public static function failed(int $errorStatus): self
     {
         return new static(null, $errorStatus);
+    }
+
+    public function message(): string
+    {
+        if ($this->amount === null) {
+            return sprintf('%d) Failed: %s', $this->status, self::MESSAGES[$this->status]);
+        }
+        return sprintf('%d) Success: %s: %f', $this->status, self::MESSAGES[$this->status], $this->amount);
     }
 }
